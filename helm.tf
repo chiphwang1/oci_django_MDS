0c7d96172c3ea08373843a5fca4911fa9bd7bb80
ociobenablement/identitycloudservice/chiping.hwang@oracle.com <test@example.com>
2022-05-03 09:18:15 -0700

    first commit

---
+++ helm.tf
@@ -0,0 +1,43 @@
+provider "helm" {
+  kubernetes {
+    config_path = "/tmp/kubeconfig"
+  }
+}
+
+
+resource "time_sleep" "wait_1min" {
+  depends_on = [
+  local_file.KubeConfigFile
+  ]
+  create_duration = "60s"
+}
+
+resource "helm_release" "magento" {
+  depends_on = [time_sleep.wait_1min, module.mds-instance]
+  name       = "magento"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "magento"
+
+  set {
+    name  = "mariadb.enabled"
+    value = false
+  }
+
+  set {
+    name = "externalDatabase.host"
+    value = module.mds-instance.mysql_db_system.ip_address
+  }
+
+  set {
+    name = "externalDatabase.password"
+    value = var.admin_password
+  }
+
+
+set {
+    name = "externalDatabase.user"
+    value = var.admin_username
+  }
+
+}
